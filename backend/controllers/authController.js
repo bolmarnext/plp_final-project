2deed64a91ff6b05f455005114530a0e2643de1f
bolmarnext <bolmarc2020@gmail.com>
2024-11-30 01:36:14 +0100

    This is my final project

---
+++ backend/controllers/authController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { username, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword, role });
+
+    try {
+        res.status(201).json({ message: 'User  created', user });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+};
